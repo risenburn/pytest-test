1e14fd165a0ad4166eb1ebe1bbaf2d59f3165c50
Nikita <77363524+risenburn@users.noreply.github.com>
2024-04-13 15:01:59 +0300

    Update test-app.py

--- test-app.py
+++ test-app.py
@@ -8,7 +8,7 @@ from time import sleep
 def browser():
     chrome_options = webdriver.ChromeOptions()
     browser = webdriver.Remote(
-           command_executor='http://172.27.0.3:4444/wd/hub',
+           command_executor='http://localhost:4444/wd/hub',
            desired_capabilities={'browserName': 'chrome',
                                   'version': '123.0'},
            options=chrome_options)
