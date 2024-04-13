538a811b6b08cb5e9099a7bd570c7bd9f1527423
Nikita <77363524+risenburn@users.noreply.github.com>
2024-04-13 13:37:39 +0300

    Update test-app.py

--- test-app.py
+++ test-app.py
@@ -1,20 +1,32 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
 
-from myapp.app import multiply_by_two, divide_by_two
+def test_google():  
+	browser.get('https://www.google.com/')
+	sleep(60)
+	title = "Google"
+	assert title == chrome_driver.title
+	pageSource = chrome_driver.page_source
+	gateway = "Our third decade of climate action"
+	if not gateway in pageSource:
+		pytest.fail( "Google Changed their Webpage")
+	chrome_driver.close()
 
 
-@pytest.fixture
-def numbers():
-    a = 10
-    b = 20
-    return [a,b]
-
-
-class TestApp:
-    def test_multiplication(self, numbers):
-        res = multiply_by_two(numbers[0])
-        assert res == numbers[1]
-
-    def test_division(self, numbers):
-        res = divide_by_two(numbers[1])
-        assert res == numbers[0]
+@pytest.fixture(scope="session")
+def browser():
+	chrome_options = webdriver.ChromeOptions()
+	browser = webdriver.Remote(
+    	   command_executor='http://selenoid:4444/wd/hub',
+    	   desired_capabilities={'browserName': 'chrome',
+                          	'version': '123.0'},
+    	   options=chrome_options)
+ 
+	browser.maximize_window()
+ 
+	yield browser
+	browser.quit()
